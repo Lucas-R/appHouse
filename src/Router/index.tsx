0e2262aee9dda5b8984290d4784f7bfa29e1dd92
Lucas Rodrigues <lucas.rodrigues@mktnow.com.br>
2024-07-29 17:45:25 -0300

    added page home

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -2,11 +2,12 @@ import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
+import { Home } from "../pages/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element:  <h1 className="text-3xl font-bold underline">Hello world!</h1>,
+        element:  <Home />,
     },
 ]);
 
